287e9dcef24ff5ad983c84089b74e5f2f2c32a8d
Patrik Opacic <opacic@gmail.com>
2025-06-09 11:44:52 +0200

    Build fix

--- circles/src/components/modules/proposals/proposal-item.tsx
+++ circles/src/components/modules/proposals/proposal-item.tsx
@@ -662,7 +662,7 @@ export const ProposalItem: React.FC<ProposalItemProps> = ({ proposal, circle, is
                     onOpenChange={setCreateGoalDialogOpen}
                     itemKey="goal"
                     proposal={proposal}
-                    circle={circle} // Pass the circle from ProposalItemProps
+                    // circle={circle} // Pass the circle from ProposalItemProps
                     onSuccess={(goalId) => {
                         toast({ title: "Success", description: "Goal created from proposal." });
                         setCreateGoalDialogOpen(false);
